87f907a7ce0903eb67c36daed000e144229314f5
abdou123touil <abdelrahman1920@gmail.com>
2026-05-04 15:41:26 +0200

    fix deploiment

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ;    //|| "http://localhost:8081/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ;//|| "http://localhost:8081/api";
 
 let token: string | null = null;
 export type SalesByDay = {
