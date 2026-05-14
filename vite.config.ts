4cd8c9c044a6c2380dfa24fec14a78204aac75bf
abdou123touil <abdelrahman1920@gmail.com>
2026-05-14 16:35:48 +0200

    fix view for tablet and mobile, add logout in pos, add change password checkbox in employee edit form

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Boulangerie Liegeois",
+        short_name: "POS",
+
+        start_url: "/",
+
+        display: "fullscreen",
+
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+
+        orientation: "portrait",
+
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
