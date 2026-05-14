9b58d24898738697113cdb979999b1ce6873c15b
abdou123touil <abdelrahman1920@gmail.com>
2026-05-14 16:43:22 +0200

    configurer vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
         background_color: "#ffffff",
         theme_color: "#ffffff",
 
-        orientation: "portrait",
+        orientation: "landscape",
 
         icons: [
           {
