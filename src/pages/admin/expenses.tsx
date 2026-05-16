d4aa48ebec6de616f5102c2b5fd08ad89edc6488
abdou123touil <abdelrahman1920@gmail.com>
2026-05-16 15:32:40 +0200

    fix expences

--- src/pages/admin/expenses.tsx
+++ src/pages/admin/expenses.tsx
@@ -192,7 +192,7 @@ export default function Expenses() {
                 animate={{ opacity: 1, scale: 1, y: 0 }}
                 exit={{ opacity: 0, scale: 0.95, y: 20 }}
                 transition={{ duration: 0.2 }}
-                className="fixed  z-50 w-[95vw] max-w-[500px] max-h-[85vh] -translate-x-1/2  overflow-y-auto rounded-2xl border border-border bg-card shadow-2xl"
+                className="fixed left-1/2 top-1/2 z-50 w-[95vw] max-w-[500px] max-h-[85vh] -translate-x-1/2 -translate-y-1/2 overflow-y-auto rounded-2xl border border-border bg-card shadow-2xl"
               >
                 <div className="sticky  z-10 flex items-center justify-between border-b border-border bg-card px-6 py-4">
                   <h2 className="text-xl font-semibold text-primary">{t('expenses.add_title')}</h2>
